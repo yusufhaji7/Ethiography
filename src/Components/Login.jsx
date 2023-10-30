7bfa6a9f94f7d9be7d37c19e41109181efd1e5b8
yusufhaji7 <yusufhadji873@gmail.com>
2023-10-30 19:47:45 +0300

    deployment absolute path

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -70,7 +70,7 @@ const Login = ({ setAuthenticated }) => {
           </div>
           <button
             type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="bg-black text-white px-4 py-2 rounded hover:bg-black focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Login
           </button>
